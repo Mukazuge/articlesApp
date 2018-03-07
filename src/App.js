b3855fb8328a675b0b108af79a4fb4657ef2c9ae
hermes <mukazuge@gmail.com>
2018-03-07 04:27:48 +0000

    [HOTFIX] removing .idea from repo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ArticleCard from "./components/card-component/card-container";
 
+const ipsum = `
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+  nisi ut aliquip ex ea commodo consequat.`;
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <ArticleCard
+                    title="Woman T-Shirt"
+                    description={ipsum}
+                    status="danger"
+                    statusText="Out of stock"/>
+            </div>
+        );
+    }
 }
 
 export default App;
